1895a4a82cae4b9d0399712d2daa32494618945a
Felipe Schulz <schulz.neo@gmail.com>
2020-05-12 09:58:40 -0300

    Organizando o template e adicionando base.css

--- centro_sul_clientes_cadastro/urls.py
+++ centro_sul_clientes_cadastro/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-     path('', views.home, name="home"),
+     path('', views.index, name="index"),
      path('cliente/<int:pk>/', views.detalhe_cliente, name="detalhe_cliente"),
      path('cliente/edit/<int:pk>/', views.editar_cliente, name="edit_cliente"),
      path('cliente/new/', views.adicionar_cliente, name="adicionar_cliente"),
